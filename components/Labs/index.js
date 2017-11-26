4758baabbe0d349128aec414a2530aad900f7275
Jepser Bernardino <jepsersk8@gmail.com>
2017-11-26 22:36:13 +0100

    chore: add active link

--- components/Labs/index.js
+++ components/Labs/index.js
@@ -36,14 +36,14 @@ export const Preview = (props) => {
         <Item>
           <PublishDate>{publishedDate}</PublishDate>
           <Title>
-            <Link href={`/lab?slug=${slug}&type=labs`} as={`/labs/${slug}`}>
+            <Link href={`/article?slug=${slug}&type=labs`} as={`/labs/${slug}`}>
               <a>{title}</a>
             </Link>
           </Title>
           <Excerpt>{excerpt}</Excerpt>
           <Meta>
             <ProjectLink href={`${projectUrl}`}>Ver proyecto</ProjectLink>
-            <Link href={`/lab?slug=${slug}&type=labs`} as={`/labs/${slug}`}>
+            <Link href={`/article?slug=${slug}&type=labs`} as={`/labs/${slug}`}>
               <ContinueReading>Ver historia</ContinueReading>
             </Link>
           </Meta>
