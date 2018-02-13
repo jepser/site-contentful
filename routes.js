90a5183bd7eb28d1c897a0fca24d5383c2743f06
Jepser Bernardino <jepsersk8@gmail.com>
2018-02-13 23:33:31 +0100

    feat: add migration script route

--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@ module.exports = () => {
   return {
     '/': { page: '/' },
     '/labs': { page: '/labs' },
+    '/articles/script-para-migration': { page: '/labs/migration-script' },
     '/articles': { page: '/articles'}
   }
 }
