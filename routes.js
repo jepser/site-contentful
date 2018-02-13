f2c3b3f47a817dd2dd7977556acd8b9f9ceb2a93
Jepser Bernardino <jepsersk8@gmail.com>
2018-02-14 00:02:00 +0100

    fix: issue with slug

--- routes.js
+++ routes.js
@@ -2,7 +2,7 @@ module.exports = () => {
   return {
     '/': { page: '/' },
     '/labs': { page: '/labs' },
-    '/articles/script-para-migration': { page: '/labs/migration-script' },
+    '/labs/script-para-migracion': { page: '/labs/migration-script' },
     '/articles': { page: '/articles'}
   }
 }
