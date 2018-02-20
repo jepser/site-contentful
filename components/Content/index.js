c545202cdcc39b4a8a0edf8d1bfa7c83babc23c8
Jepser Bernardino <jepsersk8@gmail.com>
2018-02-20 15:13:06 +0100

    fix: responsive layout

--- components/Content/index.js
+++ components/Content/index.js
@@ -1,14 +1,34 @@
 import styled from 'styled-components'
 
 const Content = styled.div`
-  margin-left: 200px;
+  margin-left: 0;
   padding: 40px;
   max-width: 800px;
   line-height: 1.625;
 
-  @media (max-width: 640px) {
-    margin-left: 0;
+  a {
+    text-decoration: underline;
   }
+
+  img {
+    max-width: 100%;
+  }
+
+  @media (min-width: 768px) {
+    margin-left: 140px;
+  }
+
+  @media (min-width: 1000px) {
+    
+    img {
+      max-width: 110%;
+    }
+  }
+
+  @media (min-width: 1024px) {
+    margin-left: 200px;
+  }
+
 `
 
 export default Content
