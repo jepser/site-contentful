0b2146e65b733ec7632885dd48e501407e7d8416
Jepser Bernardino <jepsersk8@gmail.com>
2018-03-25 12:14:11 -0500

    chore: add details for new trips

--- components/Notice/index.js
+++ components/Notice/index.js
@@ -14,6 +14,8 @@ const Notice = ()  => {
       <br />
       <p>🇨🇴 Colombia del 17 de marzo al 9 de abril.</p>
       <p>🇪🇸 Madrid el 21 de abril, speaker en <a href="https://2018.madrid.wordcamp.org/programa/" target="_blank">WordCamp Madrid</a>.</p>
+      <p>🇷🇸 Serbia en Junio para el <a href="https://2018.europe.wordcamp.org/" target="_bank">WordCamp Europe</a>.</p>
+      <p>🇨🇳 China del 7 de Septiembre al 2 de Octubre.</p>
     </Wrap>
   )
 }
