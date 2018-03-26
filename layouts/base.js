e68afc327c690393d370773ad9a2202433585dc6
Jepser Bernardino <jepsersk8@gmail.com>
2018-03-25 19:28:19 -0500

    feat: add loading animation

--- layouts/base.js
+++ layouts/base.js
@@ -1,4 +1,9 @@
+import {
+  Component
+} from 'react'
 import Head from 'next/head'
+import NProgress from 'nprogress'
+import Router from 'next/router'
 import styled from 'styled-components'
 
 import Header from '../components/Header'
@@ -6,17 +11,40 @@ import Tracking from '../components/Head'
 
 const Wrap = styled.div`
   min-height: 100vh;
+  opacity: ${ props => props.isLoading ? .6 : 1};
+  transition: opacity .3s ease;
 `
 
-export default ({ children, title, theme }) => (
-  <div>
-    {process.env.NODE_ENV === 'production' && <Tracking />}
-    <Head>
-      <title>{title}</title>
-    </Head>
-    <Header theme={theme} />
-    <Wrap>
-      { children }
-    </Wrap>
-  </div>
-)
+export default class extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      loading: false
+    }
+
+    Router.onRouteChangeStart = () => this.setState({ loading: true })
+    Router.onRouteChangeComplete = () => this.setState({ loading: false })
+    Router.onRouteChangeError = () => this.setState({ loading: false })
+  }
+
+  render() {
+    const {
+      title,
+      theme,
+      children
+    } = this.props
+    return(
+      <div>
+        {process.env.NODE_ENV === 'production' && <Tracking />}
+        <Head>
+          <title>{title}</title>
+        </Head>
+        <Header theme={theme} />
+        <Wrap isLoading={this.state.loading}>
+          {children}
+        </Wrap>
+      </div>
+    )
+  }
+}
