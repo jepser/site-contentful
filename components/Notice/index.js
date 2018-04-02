d803c59bd8912c9dc62531b9508be99b62690ffa
Jepser Bernardino <jepsersk8@gmail.com>
2018-04-02 12:30:33 -0500

    chore: update notice component

--- components/Notice/index.js
+++ components/Notice/index.js
@@ -13,7 +13,8 @@ const Notice = ()  => {
       <br />
       <br />
       <p>🇨🇴 Colombia del 17 de marzo al 9 de abril.</p>
-      <p>🇪🇸 Madrid el 21 de abril, speaker en <a href="https://2018.madrid.wordcamp.org/programa/" target="_blank">WordCamp Madrid</a>.</p>
+      <p>🇪🇸 Madrid el 20 y 21 de abril, speaker en <a href="https://2018.madrid.wordcamp.org/programa/" target="_blank">WordCamp Madrid</a>.</p>
+      <p>🇵🇹 Porto 18 y 19 de mayo, speaker en <a href="https://2018.porto.wordcamp.org/" target="_bank">WordCamp Porto</a>.</p>
       <p>🇷🇸 Serbia en Junio para el <a href="https://2018.europe.wordcamp.org/" target="_bank">WordCamp Europe</a>.</p>
       <p>🇨🇳 China del 7 de Septiembre al 2 de Octubre.</p>
     </Wrap>
