f558ab866fcabdae1d0049b9b1e71178aa2d6b9d
Jepser Bernardino <jepsersk8@gmail.com>
2020-04-05 12:27:57 +0200

    fix: font source

--- pages/_document.js
+++ pages/_document.js
@@ -2,23 +2,16 @@ import { createGlobalStyle, ServerStyleSheet } from 'styled-components'
 import Document, { Head, Main, NextScript } from 'next/document'
 
 const GlobalStyle = createGlobalStyle`
-  
-  @font-face {
-  font-family: 'IBM Plex Sans', sans-serif;
-  font-style: normal;
-  font-weight: 400;
-  src: url("/static/fonts/IBMPlexSerif-Regular-Latin1.woff2") format("woff2"), url("/static/fonts/IBMPlexSerif-Regular-Latin1.woff") format("woff");
-  unicode-range: 'U+0000-00FF, U+0131, U+0152-0153, U+02C6, U+02DA, U+02DC, U+2000-206F, U+20AC, U+2122, U+2212, U+FB01-FB02'; }
-  
+
   body {
     margin: 0;
     padding: 0;
-    font-family: 'IBM Plex Sans';
+    font-family: 'IBM Plex Sans', sans-serif;
     min-height: 100%;
   }
   
   h1, h2, h3, h4, h5, button, input {
-    font-family: 'IBM Plex Sans';
+    font-family: 'IBM Plex Sans', sans-serif;
   }
 
   html {
@@ -53,12 +46,9 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
-          <link rel='icon' href='/static/favicon.ico' />
+          <link rel='icon' href='/favicon.ico' />
           <meta charSet='utf-8' />
-          <link
-            href='https://fonts.googleapis.com/css?family=IBM+Plex+Sans'
-            rel='stylesheet'
-          />
+          <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans&display=swap" rel="stylesheet" />
           <meta
             name='viewport'
             content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'
