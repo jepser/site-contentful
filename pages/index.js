447f69d4bffa36356e7fba417fd149c5eff2bfa3
Jepser Bernardino <jepsersk8@gmail.com>
2020-05-31 17:16:45 +0200

    feat: update working experience

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ const Index = (props) => {
   return (
     <Page
       title='Jepser Bernardino | product engineer from Guatemala 🇬🇹'
-      description='Software engineer with 8 years of experience in web technologies. Currently product engineering at Badi. Teaching coding at CodeOp. Previously doing Frontend at Typeform.'
+      description='Software engineer with 8 years of experience in web technologies. Currently product engineering at Maze.design. Teaching coding at CodeOp. Previously doing Frontend at Typeform and Badi.'
     >
       <Wrap>
         <Container>
@@ -21,7 +21,6 @@ const Index = (props) => {
             <Title>Hi, I am Jepser Bernardino.</Title>
             <Title>Software engineer from Guatemala.</Title>
             <Title>I craft high-detailed & performant <br/>web apps  using the latest technologies.</Title>
-            <Title>I have collaborated with: <br />Saatchi & Saatchi, BBDO, Taco Bell & Starmedia.</Title>
             <Spacer />
           </Content>
         </Container>
@@ -32,8 +31,8 @@ const Index = (props) => {
             <Title>Co-founder of <a href='https://royalestudios.com'>royalestudios</a>.</Title>
             <Title>Currently, I live in Barcelona 🏖. </Title>
             <Title>Teaching coding at <a href="https://codeop.tech/">CodeOp</a>.</Title>
-            <Title>Product engineer at <a href='https://badi.com'>Badi.</a></Title>
-            <Title>Previously at <a href='https://www.typeform.com'>Typeform.</a></Title>
+            <Title>Fullstack engineer at <a href='https://maze.design'>Maze.design.</a></Title>
+            <Title>Previously at <a href='https://www.typeform.com'>Typeform</a> and <a href='https://badi.com'>Badi.</a></Title>
           </Content>
         </Container>
       </OppositeWrap>
@@ -42,7 +41,7 @@ const Index = (props) => {
           <Content>
             <Title>I developed a tracking tool for web-apps <a href='https://dev.to/jepser/hello-treacker-a-tracking-library-for-react-59bd'>meet Treacker</a>. </Title>
             <Title>I created the first <a href='https://www.typeform.com/blog/human-experience/cui/'>conversational article</a> in the world. </Title>
-            <Title>Launched <a href='https://labs.typeform.com/cui/'>a product</a> that creates a better data collection experience.</Title>
+            <Title>Launched <a href='https://try.typeform.com/conversations/'>a product</a> that creates a better data collection experience.</Title>
             <Title>Developed an <a href='http://spotify-youtube.jepser.com'>application</a> that converts playlist from Spotify to Youtube.</Title>
             <Title>Integrated WooCommerce and Credomatic by <a href='http://jepser.com/labs/woocommerce-credomatic/'>creating a plugin</a> that makes possible online payments in Central America.</Title>
           </Content>
